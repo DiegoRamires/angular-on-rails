fd931cab28cc0b0ce95da03e040fbf47aa40cf68
Diego Ramires <goncalves.ramires@gmail.com>
2018-04-08 10:07:12 -0300

    Creating mock data

--- freelance-camp-fe/app/documents/documents.component.ts
+++ freelance-camp-fe/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Document } from './document';
 
 @Component({
   moduleId: module.id,
@@ -6,5 +7,27 @@ import { Component } from "@angular/core";
   templateUrl: 'documents.component.html'
 })
 export class DocumentsComponent {
-  
+  documents: Document[] = [
+    {
+      title: "My first Doc",
+      description: "lorem sajdhashdalk",
+      file_url: "http://google.com",
+      updated_at: '04/08/18',
+      image_url: "http://google.com",
+    },
+    {
+      title: "My second Doc",
+      description: "lorem sajdhashdalk",
+      file_url: "http://google.com",
+      updated_at: '04/08/18',
+      image_url: "http://google.com",
+    },
+    {
+      title: "My last Doc",
+      description: "lorem sajdhashdalk",
+      file_url: "http://google.com",
+      updated_at: '04/08/18',
+      image_url: "http://google.com",
+    }
+  ]
 }
